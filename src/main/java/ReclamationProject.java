e8f8b6d14ad459081bec6731d5458547a8229b15
carson <carson@sflc>
2017-09-26 10:31:17 -0500

    Fixed a ton of style errors!

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,32 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**
+ * Finds the string which is in common between the two parameter strings.
+ */
+public class ReclamationProject {
+    /**
+     * Method to find substrings in common between a and b.
+     * @param a The first input string which will be searched for common substrings with b.
+     * @param b Second input string which will be searched for common substrings with a.
+     * @return The resultant common substrings.
+     */
+    static String doit(final String a, final String b) {
+        if (a.length() > b.length()) {
+            String c = a, newA = a, newB = b;
+            newA = b;
+            newB = c;
+        }
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    r = (a.regionMatches(i, b, k, j) && j
+                            > r.length()) ? a.substring(i, i + j) : r; // Do it!
+                }
+            } // Ah yeah
         } return r; }
 }
